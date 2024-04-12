4511706216f519bc6665bb40e19037babd8dde82
McJafet <jafet2195@gmail.com>
2024-04-12 18:19:43 -0500

    Pushing to GitHub

---
+++ Practicando/script.js
@@ -0,0 +1,12 @@
+import React from "https://esm.sh/react@18.2.0"
+import ReactDOM from "https://esm.sh/react-dom@18.2.0/client"
+
+const appDomElement = document.getElementById('app')
+const root = ReactDOM.createRoot(appDomElement)
+root.render('hola mundo')
+
+if (true) {
+    if (true) {
+        
+    }
+}
